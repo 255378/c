6fbac27f1317689e6f01553becebca8ed565dd3b
student <student@p.lodz.pl>
2025-04-07 13:56:14 +0200

    menu added

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 
+void menu(void) {
+    printf("\n");
+    printf("0 - Exit\n");
+    printf("1 - Add\n");
+    printf("2 - Subtract\n");
+    printf("3 - Multiply\n");
+    printf("4 - Divide\n");
+    printf("Select an option:\n");
+
 int main(void) {
     printf("Simple Calc\n");
     return 0;
